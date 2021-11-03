ba1a36eca34ab53feea5d9000338f8686599f1e7
Bartosz Drozd <drozdiibard@gmail.com>
2021-11-03 14:36:56 +0000

    Rewrite error handling of the diabetes form

--- app/src/main/java/com/bartoszdrozd/mediapp/forms/ui/health/DiabetesFormFragment.kt
+++ app/src/main/java/com/bartoszdrozd/mediapp/forms/ui/health/DiabetesFormFragment.kt
@@ -11,7 +11,6 @@ import androidx.fragment.app.viewModels
 import com.bartoszdrozd.mediapp.databinding.FragmentDiabetesFormBinding
 import com.bartoszdrozd.mediapp.forms.viewmodels.DiabetesFormViewModel
 import com.bartoszdrozd.mediapp.forms.dtos.DiabetesFormDTO
-import com.bartoszdrozd.mediapp.forms.models.health.DiabetesFormField.*
 import com.bartoszdrozd.mediapp.forms.models.health.FormErrorCode
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -43,88 +42,60 @@ class DiabetesFormFragment : Fragment() {
         }
     }
 
-    private fun clearErrors() {
-        with(binding) {
-            pregnancies.error = ""
-            glucose.error = ""
-            insulin.error = ""
-            bloodPressure.error = ""
-            skinThickness.error = ""
-            bmi.error = ""
-        }
-    }
-
     private fun setListeners() {
         with(viewModel) {
             isPregnanciesFieldVisible.observe(viewLifecycleOwner, { isVisible ->
                 binding.pregnancies.visibility = if (isVisible) VISIBLE else GONE
             })
+            binding.buttonSave.setOnClickListener {
+                val pregnancies = binding.pregnanciesText.text.toString().trim()
+                val glucose = binding.glucoseText.text.toString().trim()
+                val insulin = binding.insulinText.text.toString().trim()
+                val bloodPressure = binding.bloodPressureText.text.toString().trim()
+                val skinThickness = binding.skinThicknessText.text.toString().trim()
+                val bmi = binding.bmiText.text.toString().trim()
 
-            formErrors.observe(viewLifecycleOwner, { errors ->
-                clearErrors()
-                for (error in errors) {
-                    when (error.field) {
-                        PREGNANCIES -> binding.pregnancies.error =
-                            getErrorString(error.errorCode)
-                        GLUCOSE_LEVELS -> binding.glucose.error =
-                            getErrorString(error.errorCode)
-                        INSULIN_LEVELS -> binding.insulin.error =
-                            getErrorString(error.errorCode)
-                        BLOOD_PRESSURE -> binding.bloodPressure.error =
-                            getErrorString(error.errorCode)
-                        SKIN_THICKNESS -> binding.skinThickness.error =
-                            getErrorString(error.errorCode)
-                        BMI -> binding.bmi.error =
-                            getErrorString(error.errorCode)
-                        ERROR_BOX -> binding.errorBox.text =
-                            getErrorString(error.errorCode)
-                    }
-                }
-            })
-        }
-        binding.buttonSave.setOnClickListener {
-            val pregnancies = binding.pregnanciesText.text.toString().trim()
-            val glucose = binding.glucoseText.text.toString().trim()
-            val insulin = binding.insulinText.text.toString().trim()
-            val bloodPressure = binding.bloodPressureText.text.toString().trim()
-            val skinThickness = binding.skinThicknessText.text.toString().trim()
-            val bmi = binding.bmiText.text.toString().trim()
-
-            viewModel.saveForm(
-                DiabetesFormDTO(
-                    pregnancies.toIntOrNull(),
-                    glucose.toIntOrNull(),
-                    insulin.toIntOrNull(),
-                    bloodPressure.toIntOrNull(),
-                    skinThickness.toIntOrNull(),
-                    bmi.toIntOrNull()
+                viewModel.saveForm(
+                    DiabetesFormDTO(
+                        pregnancies.toIntOrNull(),
+                        glucose.toIntOrNull(),
+                        insulin.toIntOrNull(),
+                        bloodPressure.toIntOrNull(),
+                        skinThickness.toIntOrNull(),
+                        bmi.toIntOrNull()
+                    )
                 )
-            )
-        }
-//            pregnanciesError.observe(viewLifecycleOwner, { error ->
-//                binding.pregnancies.error = getErrorString(error)
-//            })
-//
-//            glucoseError.observe(viewLifecycleOwner, { error ->
-//                binding.glucose.error = getErrorString(error)
-//            })
-//
-//            insulinError.observe(viewLifecycleOwner, { error ->
-//                binding.insulin.error = getErrorString(error)
-//            })
-//
-//            bloodPressureError.observe(viewLifecycleOwner, { error ->
-//                binding.bloodPressure.error = getErrorString(error)
-//            })
-//
-//            skinThicknessError.observe(viewLifecycleOwner, { error ->
-//                binding.skinThickness.error = getErrorString(error)
-//            })
-//
-//            bmiError.observe(viewLifecycleOwner, { error ->
-//                binding.bmi.error = getErrorString(error)
-//            })
+            }
+            pregnanciesError.observe(viewLifecycleOwner, { error ->
+                binding.pregnancies.error = getErrorString(error)
+            })
 
+            glucoseError.observe(viewLifecycleOwner, { error ->
+                binding.glucose.error = getErrorString(error)
+            })
+
+            insulinError.observe(viewLifecycleOwner, { error ->
+                binding.insulin.error = getErrorString(error)
+            })
+
+            bloodPressureError.observe(viewLifecycleOwner, { error ->
+                binding.bloodPressure.error = getErrorString(error)
+            })
+
+            skinThicknessError.observe(viewLifecycleOwner, { error ->
+                binding.skinThickness.error = getErrorString(error)
+            })
+
+            bmiError.observe(viewLifecycleOwner, { error ->
+                binding.bmi.error = getErrorString(error)
+            })
+
+            generalError.observe(viewLifecycleOwner, { error ->
+                binding.errorBox.text = getErrorString(error)
+            })
+
+
+        }
 
     }
 
--- app/src/main/java/com/bartoszdrozd/mediapp/forms/viewmodels/DiabetesFormViewModel.kt
+++ app/src/main/java/com/bartoszdrozd/mediapp/forms/viewmodels/DiabetesFormViewModel.kt
@@ -6,8 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.bartoszdrozd.mediapp.auth.repositories.IUsersRepository
 import com.bartoszdrozd.mediapp.forms.dtos.DiabetesFormDTO
-import com.bartoszdrozd.mediapp.forms.models.health.DiabetesFormField
-import com.bartoszdrozd.mediapp.forms.models.health.FormError
 import com.bartoszdrozd.mediapp.forms.models.health.FormErrorCode
 import com.bartoszdrozd.mediapp.forms.usecases.ISaveDiabetesFormUseCase
 import com.bartoszdrozd.mediapp.utils.Error
@@ -23,12 +21,32 @@ class DiabetesFormViewModel @Inject constructor(
 ) :
     ViewModel() {
     private val _isPregnanciesFieldVisible = MutableLiveData(false)
-    private val _formErrors = MutableLiveData<List<FormError>>(listOf())
+    private val _pregnanciesError = MutableLiveData<FormErrorCode>()
+    private val _glucoseError = MutableLiveData<FormErrorCode>()
+    private val _insulinError = MutableLiveData<FormErrorCode>()
+    private val _bloodPressureError = MutableLiveData<FormErrorCode>()
+    private val _skinThicknessError = MutableLiveData<FormErrorCode>()
+    private val _bmiError = MutableLiveData<FormErrorCode>()
+    private val _generalError: MutableLiveData<FormErrorCode> = MutableLiveData()
+
+    private val isFormValid: Boolean
+        get() {
+            return _pregnanciesError.value == null || _glucoseError.value == null ||
+                    insulinError.value == null || _bloodPressureError.value == null ||
+                    _skinThicknessError.value == null || _bmiError.value == null
+        }
 
     val isPregnanciesFieldVisible: LiveData<Boolean> = _isPregnanciesFieldVisible
-    val formErrors: LiveData<List<FormError>> = _formErrors
+    val pregnanciesError: LiveData<FormErrorCode> = _pregnanciesError
+    val glucoseError: LiveData<FormErrorCode> = _glucoseError
+    val insulinError: LiveData<FormErrorCode> = _insulinError
+    val bloodPressureError: LiveData<FormErrorCode> = _bloodPressureError
+    val skinThicknessError: LiveData<FormErrorCode> = _skinThicknessError
+    val bmiError: LiveData<FormErrorCode> = _bmiError
+    val generalError: LiveData<FormErrorCode> = _generalError
 
     init {
+        // Change to nav args
         viewModelScope.launch {
             val user = repository.getCurrentUser()
             _isPregnanciesFieldVisible.value = user?.details?.sex != 0
@@ -36,9 +54,6 @@ class DiabetesFormViewModel @Inject constructor(
     }
 
     fun saveForm(form: DiabetesFormDTO) {
-        // Clear any previous errors
-        _formErrors.value = listOf()
-
         validatePregnancies(form.pregnancies)
         validateGlucose(form.glucoseLevel)
         validateInsulin(form.insulinLevel)
@@ -46,55 +61,40 @@ class DiabetesFormViewModel @Inject constructor(
         validateSkinThickness(form.skinThickness)
         validateBmi(form.bmi)
 
-        if (_formErrors.value!!.isEmpty()) {
+        if (isFormValid) {
             viewModelScope.launch {
                 val res = saveFormUseCase.execute(form)
                 if (res is Success) {
                     // Navigate
                 } else {
-                    addValidationError(DiabetesFormField.ERROR_BOX, (res as Error).reason)
+                    _generalError.value = (res as Error).reason
                 }
             }
         }
     }
 
-    private fun addValidationError(field: DiabetesFormField, error: FormErrorCode) {
-        _formErrors.value = _formErrors.value!!.plus(FormError(field, error))
-    }
-
     private fun validatePregnancies(count: Int?) {
-        if (count == null && isPregnanciesFieldVisible.value!!) {
-            addValidationError(DiabetesFormField.PREGNANCIES, FormErrorCode.REQUIRED_FIELD)
-        }
+        _pregnanciesError.value =
+            if (count == null && isPregnanciesFieldVisible.value!!) FormErrorCode.REQUIRED_FIELD else null
     }
 
     private fun validateGlucose(level: Int?) {
-        if (level == null) {
-            addValidationError(DiabetesFormField.GLUCOSE_LEVELS, FormErrorCode.REQUIRED_FIELD)
-        }
+        _glucoseError.value = if (level == null) FormErrorCode.REQUIRED_FIELD else null
     }
 
     private fun validateInsulin(level: Int?) {
-        if (level == null) {
-            addValidationError(DiabetesFormField.INSULIN_LEVELS, FormErrorCode.REQUIRED_FIELD)
-        }
+        _insulinError.value = if (level == null) FormErrorCode.REQUIRED_FIELD else null
     }
 
     private fun validateBloodPressure(level: Int?) {
-        if (level == null) {
-            addValidationError(DiabetesFormField.BLOOD_PRESSURE, FormErrorCode.REQUIRED_FIELD)
-        }
+        _bloodPressureError.value = if (level == null) FormErrorCode.REQUIRED_FIELD else null
     }
 
     private fun validateSkinThickness(measurement: Int?) {
-        if (measurement == null) {
-            addValidationError(DiabetesFormField.SKIN_THICKNESS, FormErrorCode.REQUIRED_FIELD)
-        }
+        _skinThicknessError.value = if (measurement == null) FormErrorCode.REQUIRED_FIELD else null
     }
 
     private fun validateBmi(value: Int?) {
-        if (value == null) {
-            addValidationError(DiabetesFormField.BMI, FormErrorCode.REQUIRED_FIELD)
-        }
+        _bmiError.value = if (value == null) FormErrorCode.REQUIRED_FIELD else null
     }
 }
